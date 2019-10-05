4eaccd4e462579731c1eba494e4b06b0f694d3d4
Pelingul <pelinbcinar@gmail.com>
2019-10-05 21:32:24 +0300

    MessageSend service is connected

--- app/src/main/java/com/example/festivalvar/ui/messages/messagedetail/MessageDetailActivity.kt
+++ app/src/main/java/com/example/festivalvar/ui/messages/messagedetail/MessageDetailActivity.kt
@@ -3,7 +3,10 @@ package com.example.festivalvar.ui.messages.messagedetail
 import android.view.View
 import com.example.festivalvar.R
 import com.example.festivalvar.data.remote.model.messages.MessageIndex
+import com.example.festivalvar.data.remote.model.messages.sendmodel.MessageSendModelRequest
 import com.example.festivalvar.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_message_detail.*
+import kotlinx.android.synthetic.main.register_screen.*
 import kotlinx.android.synthetic.main.toolbar_layout.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -24,13 +27,25 @@ class MessageDetailActivity : BaseActivity(), IMessageDetailNavigator {
 
     override fun initUI() {
 
+        iv_back.visibility = View.VISIBLE
         tvToolbarTitle.visibility = View.VISIBLE
         tvToolbarTitle.text = messageModel.user_two?.full_name
+
 
 
     }
 
     override fun initListener() {
+        iv_back.setOnClickListener {
+            finish()
+        }
+
+        icSendMessage.setOnClickListener {
+            val etMessage = etWriteMessage.text.toString()
+            val request = MessageSendModelRequest( "$etMessage", messageModel.user_two_id!!)
+            viewModel.postSendMessage(request)
+
+        }
     }
 
 }
--- app/src/main/java/com/example/festivalvar/ui/messages/messagedetail/MessageDetailViewModel.kt
+++ app/src/main/java/com/example/festivalvar/ui/messages/messagedetail/MessageDetailViewModel.kt
@@ -1,7 +1,40 @@
 package com.example.festivalvar.ui.messages.messagedetail
 
+import androidx.lifecycle.MutableLiveData
+import com.example.festivalvar.data.remote.model.messages.MessageIndex
+import com.example.festivalvar.data.remote.model.messages.sendmodel.MessageSendModel
+import com.example.festivalvar.data.remote.model.messages.sendmodel.MessageSendModelRequest
+import com.example.festivalvar.data.remote.network.ResultWrapper
 import com.example.festivalvar.data.repository.DataManager
 import com.example.festivalvar.ui.base.BaseViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MessageDetailViewModel(dataManager: DataManager): BaseViewModel<IMessageDetailNavigator>(dataManager) {
+
+
+    val messageSendDataList: MutableLiveData<MessageSendModel> = MutableLiveData()
+
+    fun postSendMessage(request: MessageSendModelRequest) {
+        getNavigator()?.showLoading()
+        GlobalScope.launch(Dispatchers.Main) {
+            when (val result = withContext(Dispatchers.IO) { dataManager.postSendMessageAsync(request) }) {
+                is ResultWrapper.Success -> {
+                    getNavigator()?.hideLoading()
+                    messageSendDataList.value = result.data.data
+
+                }
+                is ResultWrapper.Error -> {
+                    getNavigator()?.hideLoading()
+                    getNavigator()?.onError(
+                        result.exception.message,
+                        result.exception.code
+                    )
+
+                }
+            }
+        }
+    }
 }
